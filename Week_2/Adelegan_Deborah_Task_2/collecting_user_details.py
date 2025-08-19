72c0fc4a2cef2a31110557193b0af07be62e84bc
Deborah <deborahjesudemilade@gmail.com>
2025-08-19 11:45:08 +0100

    Rearranged the files correctly

--- Week_2/Adelegan_Deborah_Task_2/collecting_user_details.py
+++
@@ -1,4 +0,0 @@
-FName = input("Kindly enter your first name here: ")  # This code requests for the name of the user
-age = input("Kindly enter your age here: ")    # This code requests for the age of the user
-print(f"Welcome {FName}, you are {age} years old")  # This displays the a welcome message with the name and the age of the user
-
