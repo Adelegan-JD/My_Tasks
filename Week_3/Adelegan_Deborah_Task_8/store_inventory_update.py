2ff22601a44f7223e47b3cda0482e80bf72e2587
Deborah <deborahjesudemilade@gmail.com>
2025-08-25 09:58:26 +0100

    Added new files

--- Week_3/Adelegan_Deborah_Task_8/store_inventory_update.py
+++ Week_3/Adelegan_Deborah_Task_8/store_inventory_update.py
@@ -2,7 +2,7 @@ store = {'Bottle': 15, 'Kettle': 10, 'Jug': 35, 'Plate': 5}
 items = input("What item do you want to buy?: ")
 amount = input("How many would you like to purchase?: ")
 new_item = {items: amount}
-store -= new_item
+
 print(f"Before purchase : {store}\nAfter purchase: {store}.")
 
 
